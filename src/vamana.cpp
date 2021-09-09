8b1019a70891575d1f06777d3176e934d59cf577
ravishankar <rakri@microsoft.com>
2021-09-09 17:40:17 +0530

    trying to add incremental indexing as the static build

--- src/vamana.cpp
+++ src/vamana.cpp
@@ -337,230 +337,59 @@ namespace grann {
       }
     }
   }
-  /* Link():
-   * The graph creation function.
-   *    The graph will be updated periodically in NUM_SYNCS batches
-   */
+
   template<typename T>
-  void Vamana<T>::link(Parameters &parameters) {
-    unsigned NUM_THREADS = parameters.Get<unsigned>("num_threads");
-    if (NUM_THREADS != 0)
-      omp_set_num_threads(NUM_THREADS);
+  void Vamana<T>::build(Parameters & build_parameters) {
+    grann::cout << "Starting vamana build..." << std::endl;
 
-    uint32_t NUM_SYNCS =
-        (unsigned) DIV_ROUND_UP(this->_num_points + this->_num_steiner_pts, (64 * 64));
-    if (NUM_SYNCS < 40)
-      NUM_SYNCS = 40;
-    grann::cout << "Number of syncs: " << NUM_SYNCS << std::endl;
 
-    if (NUM_THREADS != 0)
-      omp_set_num_threads(NUM_THREADS);
+    unsigned num_threads = parameters.Get<unsigned>("num_threads");
+    unsigned list_size = parameters.Get<unsigned>("L");
+    unsigned degree_budget = parameters.Get<unsigned>("R");
+    float alpha = parameters.Get<float>("alpha");
 
-    const unsigned argL = parameters.Get<unsigned>("L");  // Search list size
-    const unsigned range = parameters.Get<unsigned>("R");
-    const float    last_round_alpha = parameters.Get<float>("alpha");
-    unsigned       L = argL;
+    if (num_threads != 0)
+      omp_set_num_threads(num_threads);
 
-    std::vector<unsigned> Lvec;
-    Lvec.push_back(L);
-    Lvec.push_back(L);
-    const unsigned NUM_RNDS = 2;
 
-    // Max degree of graph
-    // Pruning parameter
-    // Set alpha=1 for the first pass; use specified alpha for last pass
-    parameters.Set<float>("alpha", 1);
+    #pragma omp parallel for schedule (static, 64)
+for (_u32 location = 0; location < this->_num_points; location++) {
+    auto offset_data = this->_data + (size_t) this->_aligned_dim * location;
 
-    /* visit_order is a vector that is initialized to the entire graph */
-    std::vector<unsigned> visit_order;
-    visit_order.reserve(this->_num_points + this->_num_steiner_pts);
-    for (unsigned i = 0; i < (unsigned) this->_num_points; i++) {
-      visit_order.emplace_back(i);
+    std::vector<Neighbor> pool;
+    std::vector<Neighbor> tmp;
+    tsl::robin_set<_u32> visited;
+    pool.reserve(2*list_size);
+    tmp.reserve(2*list_size);
+    visited.reserve(20*list_size);
+
+    std::vector<_u32> pruned_list;
+    std::vector<_u32> init_ids;
+    get_expanded_nodes(location, list_size, init_ids, pool, visited);
+
+    for (unsigned i = 0; i < pool.size(); i++)
+/*      if (pool[i].id == location) {
+        pool.erase(pool.begin() + i);
+        visited.erase(location);
+        break;
+      }*/
+    prune_neighbors(location, pool, parameters, pruned_list);
+
+    _out_nbrs[location].reserve((_u64)(VAMANA_SLACK_FACTOR*range));
+    for (auto link : pruned_list) {
+      _out_nbrs[location].emplace_back(link);
     }
+    inter_insert(location, pruned_list, parameters, 0);
 
-    for (unsigned i = 0; i < (unsigned) this->_num_steiner_pts; ++i)
-      visit_order.emplace_back((unsigned) (this->_num_points + i));
+    return 0;
 
-    // if there are frozen points, the first such one is set to be the _start_node
-    if (this->_num_steiner_pts > 0)
-      this->_start_node = (unsigned) this->_num_points;
-    else
-      this->_start_node = calculate_entry_point();
 
-    this->_out_nbrs.reserve(_num_points + _num_steiner_pts);
-    this->_out_nbrs.resize(_num_points + _num_steiner_pts);
-
-    for (uint64_t p = 0; p < this->_num_points + this->_num_steiner_pts; p++) {
-      this->_out_nbrs[p].reserve((size_t)(std::ceil(range * VAMANA_SLACK_FACTOR * 1.05)));
-    }
-
-    std::random_device               rd;
-    std::mt19937                     gen(rd());
-    std::uniform_real_distribution<> dis(0, 1);
-
-    // creating a initial list to begin the search process. it has _start_node and
-    // random other nodes
-    std::set<unsigned> unique_start_points;
-    unique_start_points.insert(this->_start_node);
-
-    std::vector<unsigned> init_ids;
-    for (auto pt : unique_start_points)
-      init_ids.emplace_back(pt);
-
-    grann::Timer link_timer;
-    for (uint32_t rnd_no = 0; rnd_no < NUM_RNDS; rnd_no++) {
-      L = Lvec[rnd_no];
-
-      if (rnd_no == NUM_RNDS - 1) {
-        if (last_round_alpha > 1)
-          parameters.Set<float>("alpha", last_round_alpha);
-      }
-
-      double   sync_time = 0, total_sync_time = 0;
-      double   inter_time = 0, total_inter_time = 0;
-      size_t   inter_count = 0, total_inter_count = 0;
-      unsigned progress_counter = 0;
-
-      size_t round_size = DIV_ROUND_UP(_num_points, NUM_SYNCS);  // size of each batch
-      std::vector<unsigned> need_to_sync(this->_num_points + this->_num_steiner_pts, 0);
-
-      std::vector<std::vector<unsigned>> pruned_list_vector(round_size);
-
-      for (uint32_t sync_num = 0; sync_num < NUM_SYNCS; sync_num++) {
-        size_t start_id = sync_num * round_size;
-        size_t end_id =
-            (std::min)(this->_num_points + this->_num_steiner_pts, (sync_num + 1) * round_size);
-
-        auto s = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> diff;
-
-#pragma omp parallel for schedule(dynamic)
-        for (_s64 node_ctr = (_s64) start_id; node_ctr < (_s64) end_id;
-             ++node_ctr) {
-          auto                     node = visit_order[node_ctr];
-          size_t                   node_offset = node_ctr - start_id;
-          tsl::robin_set<unsigned> visited;
-          std::vector<unsigned> &pruned_list = pruned_list_vector[node_offset];
-          // get nearest neighbors of n in tmp. pool contains all the
-          // points that were checked along with their distance from
-          // n. visited contains all the points visited, just the ids
-          std::vector<Neighbor> pool;
-          pool.reserve(L * 2);
-          visited.reserve(L * 2);
-          get_expanded_nodes(node, L, init_ids, pool, visited);
-          /* check the neighbors of the query that are not part of
-           * visited, check their distance to the query, and add it to
-           * pool.
-           */
-          if (!this->_out_nbrs[node].empty())
-            for (auto id : this->_out_nbrs[node]) {
-              if (visited.find(id) == visited.end() && id != node) {
-                float dist =
-                    this->_distance->compare(this->_data + this->_aligned_dim * (size_t) node,
-                                       this->_data + this->_aligned_dim * (size_t) id,
-                                       (unsigned) this->_aligned_dim);
-                pool.emplace_back(Neighbor(id, dist, true));
-                visited.insert(id);
-              }
-            }
-          prune_neighbors(node, pool, parameters, pruned_list);
-        }
-        diff = std::chrono::high_resolution_clock::now() - s;
-        sync_time += diff.count();
-
-// prune_neighbors will check pool, and remove some of the points and
-// create a cut_graph, which contains neighbors for point n
-#pragma omp parallel for schedule(dynamic, 64)
-        for (_s64 node_ctr = (_s64) start_id; node_ctr < (_s64) end_id;
-             ++node_ctr) {
-          _u64                   node = visit_order[node_ctr];
-          size_t                 node_offset = node_ctr - start_id;
-          std::vector<unsigned> &pruned_list = pruned_list_vector[node_offset];
-          _out_nbrs[node].clear();
-          for (auto id : pruned_list)
-            _out_nbrs[node].emplace_back(id);
-        }
-        s = std::chrono::high_resolution_clock::now();
-
-#pragma omp parallel for schedule(dynamic, 64)
-        for (_s64 node_ctr = start_id; node_ctr < (_s64) end_id; ++node_ctr) {
-          auto                   node = visit_order[node_ctr];
-          _u64                   node_offset = node_ctr - start_id;
-          std::vector<unsigned> &pruned_list = pruned_list_vector[node_offset];
-          batch_inter_insert(node, pruned_list, parameters, need_to_sync);
-          //          inter_insert(node, pruned_list, parameters, 0);
-          pruned_list.clear();
-          pruned_list.shrink_to_fit();
-        }
-
-#pragma omp parallel for schedule(dynamic, 65536)
-        for (_s64 node_ctr = 0; node_ctr < (_s64)(visit_order.size());
-             node_ctr++) {
-          auto node = visit_order[node_ctr];
-          if (need_to_sync[node] != 0) {
-            need_to_sync[node] = 0;
-            inter_count++;
-            tsl::robin_set<unsigned> dummy_visited(0);
-            std::vector<Neighbor>    dummy_pool(0);
-            std::vector<unsigned>    new_out_neighbors;
-
-            for (auto cur_nbr : _out_nbrs[node]) {
-              if (dummy_visited.find(cur_nbr) == dummy_visited.end() &&
-                  cur_nbr != node) {
-                float dist =
-                    _distance->compare(_data + _aligned_dim * (size_t) node,
-                                       _data + _aligned_dim * (size_t) cur_nbr,
-                                       (unsigned) _aligned_dim);
-                dummy_pool.emplace_back(Neighbor(cur_nbr, dist, true));
-                dummy_visited.insert(cur_nbr);
-              }
-            }
-            prune_neighbors(node, dummy_pool, parameters, new_out_neighbors);
-
-            _out_nbrs[node].clear();
-            for (auto id : new_out_neighbors)
-              _out_nbrs[node].emplace_back(id);
-          }
-        }
-
-        diff = std::chrono::high_resolution_clock::now() - s;
-        inter_time += diff.count();
-
-        if ((sync_num * 100) / NUM_SYNCS > progress_counter) {
-          grann::cout.precision(4);
-          grann::cout << "Completed  (round: " << rnd_no
-                        << ", sync: " << sync_num << "/" << NUM_SYNCS
-                        << " with L " << L << ")"
-                        << " sync_time: " << sync_time << "s"
-                        << "; inter_time: " << inter_time << "s" << std::endl;
-
-          total_sync_time += sync_time;
-          total_inter_time += inter_time;
-          total_inter_count += inter_count;
-          sync_time = 0;
-          inter_time = 0;
-          inter_count = 0;
-          progress_counter += 5;
-        }
-      }
-// Gopal. Splittng grann_dll into separate DLLs for search and build.
-// This code should only be available in the "build" DLL.
-#ifdef GRANN_BUILD
-      MallocExtension::instance()->ReleaseFreeMemory();
-#endif
-      grann::cout << "Completed Pass " << rnd_no << " of data using L=" << L
-                    << " and alpha=" << parameters.Get<float>("alpha")
-                    << ". Stats: ";
-      grann::cout << "search+prune_time=" << total_sync_time
-                    << "s, inter_time=" << total_inter_time
-                    << "s, inter_count=" << total_inter_count << std::endl;
-    }
 
     grann::cout << "Starting final cleanup.." << std::flush;
 #pragma omp parallel for schedule(dynamic, 65536)
     for (_s64 node_ctr = 0; node_ctr < (_s64)(visit_order.size()); node_ctr++) {
-      auto node = visit_order[node_ctr];
-      if (_out_nbrs[node].size() > range) {
+      auto node = node_ctr;
+      if (_out_nbrs[node].size() > degree_budget) {
         tsl::robin_set<unsigned> dummy_visited(0);
         std::vector<Neighbor>    dummy_pool(0);
         std::vector<unsigned>    new_out_neighbors;
@@ -569,9 +398,9 @@ namespace grann {
           if (dummy_visited.find(cur_nbr) == dummy_visited.end() &&
               cur_nbr != node) {
             float dist =
-                _distance->compare(_data + _aligned_dim * (size_t) node,
-                                   _data + _aligned_dim * (size_t) cur_nbr,
-                                   (unsigned) _aligned_dim);
+                this->_distance->compare(this->_data + this->_aligned_dim * (size_t) node,
+                                   this->_data + this->_aligned_dim * (size_t) cur_nbr,
+                                   (unsigned) this->_aligned_dim);
             dummy_pool.emplace_back(Neighbor(cur_nbr, dist, true));
             dummy_visited.insert(cur_nbr);
           }
@@ -583,46 +412,9 @@ namespace grann {
           _out_nbrs[node].emplace_back(id);
       }
     }
-    grann::cout << "done. Link time: "
+    grann::cout << "done. Build time: "
                   << ((double) link_timer.elapsed() / (double) 1000000) << "s"
                   << std::endl;
-  }
-
-  template<typename T>
-  void Vamana<T>::build(Parameters &             parameters,
-                             const std::vector<TagT> &tags) {
-    if (_enable_tags) {
-      if (tags.size() != _num_points) {
-        std::cerr << "#Tags should be equal to #points" << std::endl;
-        throw grann::ANNException("#Tags must be equal to #points", -1,
-                                    __FUNCSIG__, __FILE__, __LINE__);
-      }
-      for (size_t i = 0; i < tags.size(); ++i) {
-        _tag_to_location[tags[i]] = (unsigned) i;
-        _location_to_tag[(unsigned) i] = tags[i];
-      }
-    }
-    grann::cout << "Starting vamana build..." << std::endl;
-    link(parameters);  // Primary func for creating graph
-
-    if (_support_eager_delete) {
-      update_in_nbrs();  // copying values to in_graph
-    }
-
-    size_t max = 0, min = 1 << 30, total = 0, cnt = 0;
-    for (size_t i = 0; i < _num_points; i++) {
-      auto &pool = _out_nbrs[i];
-      max = (std::max)(max, pool.size());
-      min = (std::min)(min, pool.size());
-      total += pool.size();
-      if (pool.size() < 2)
-        cnt++;
-    }
-    grann::cout << "Degree: max:" << max
-                  << "  avg:" << (float) total / (float) _num_points << "  min:" << min
-                  << "  count(deg<2):" << cnt << "\n"
-                  << "Vamana built." << std::endl;
-    _max_degree = (std::max)((unsigned) max, _max_degree);
     _has_built = true;
   }
 
